afc3d526ed7bc48361a7b6879eaacd83b284c31d
Kaoru <sisbori573@gmail.com>
2022-12-13 10:11:13 +0900

    first commit

---
+++ q14/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        if(args.length > 0){
+            String bef = args[0];
+            String aft = bef.split(","); // カンマで
+        }
+         
+    }
+}
